8034c42a0bbc3d0c3fd27bd471546e32feb2db5f
Felipe G. Guedes <felipe.guedes@acad.pucrs.br>
2017-08-18 18:43:27 -0300

    Basic Rover

--- src/Rover.java
+++ src/Rover.java
@@ -1,8 +1,36 @@
 
 public class Rover {
 
+	int x;
+	int y;
+	char direction;
+	
 	public Rover() {
-		// TODO Auto-generated constructor stub
+		
+	}
+	public Rover(int x, int y, char direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public char getDirection() {
+		return direction;
+	}
+	public void setDirection(char direction) {
+		this.direction = direction;
 	}
 
 }
