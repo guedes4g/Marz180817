793ce195f9b878880753fe47cb5f6eeb74d80dc7
Rafael Victor Ruwer Araujo <rafaelruweraraujo@gmail.com>
2017-08-19 01:39:37 -0300

    Basic RoverManager created

--- src/RoverManager.java
+++ src/RoverManager.java
@@ -1,8 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
 
 public class RoverManager {
-
-	public RoverManager() {
-		// TODO Auto-generated constructor stub
+	private List<Rover> rovers;
+	private int width;
+	private int height;
+	
+	public RoverManager(int width, int height) {
+		this.rovers = new ArrayList<Rover>();
+		this.width = width;
+		this.height = height;
 	}
-
+	
+	public boolean validatePosition(int x, int y) {
+		// off the board
+		if (x > this.width - 1 || x < 0)
+			return false;
+		
+		if (y > this.height - 1 || x < 0)
+			return false;
+		
+		// another rover
+		if (this.isPositionFree(x, y))
+			return true;
+		
+		return false;
+	}
+	
+	public void addRover(int x, int y, char direction, String commands) {
+		Rover rover = new Rover(x, y, direction);
+		this.rovers.add(rover);
+		
+		rover.evaluateCommands(commands);
+	}
+	
+	private boolean isPositionFree(int x, int y) {
+		for (Rover r : this.rovers) {
+			if (r.x == x && r.y == y)
+				return false;
+		}
+		
+		return true;
+	}
 }
